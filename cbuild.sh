1983531f1ad09bcb52d6a370e370380fbfc5ad5c
Reuben Lifshay <rlifshay@gmail.com>
2019-08-11 22:50:22 -0700

    Update to be based on wine container.

--- cbuild.sh
+++ cbuild.sh
@@ -1,33 +1,26 @@
 #!/bin/sh
 set -e
 
-: ${VERSION:=11_1}
+: ${VERSION:=11.1}
 
-ctr=$(buildah from ubuntu:18.04)
+ctr=$(buildah from wine:3.0)
 
-buildah run --network host $ctr sh -c '
-	export DEBIAN_FRONTEND=noninteractive
-	dpkg --add-architecture i386 && \
-	apt-get update && \
-	apt-get -y install wine-stable'
-buildah run $ctr sh -c "[ -d /var/lib/apt/lists ] && rm -rf /var/lib/apt/lists/*"
+WINEPREFIX=/opt/dngconverter
+buildah config \
+	--env WINEPREFIX="$WINEPREFIX" \
+	--env WINEDEBUG=-all \
+	$ctr
 
-buildah config --env WINEPREFIX=/opt/dngconverter $ctr
-
-buildah add $ctr "http://download.adobe.com/pub/adobe/dng/win/DNGConverter_${VERSION}.exe" /tmp/
-buildah run $ctr wine /tmp/DNGConverter_${VERSION}.exe /S
-buildah run $ctr rm -f /tmp/DNGConverter_${VERSION}.exe
+buildah add $ctr "http://download.adobe.com/pub/adobe/dng/win/DNGConverter_$(echo "$VERSION" | tr . _).exe" /tmp/
+buildah run $ctr wine "/tmp/DNGConverter_$(echo "$VERSION" | tr . _).exe" /S
+buildah run $ctr rm -f "/tmp/DNGConverter_$(echo "$VERSION" | tr . _).exe"
 
 buildah config \
 	--entrypoint '["/usr/bin/wine", "/opt/dngconverter/drive_c/Program Files/Adobe/Adobe DNG Converter/Adobe DNG Converter.exe"]' \
 	--cmd "-c" \
 	--volume /images \
 	--workingdir /images \
-	--env WINEDEBUG=-all \
 	$ctr
 
-# bug workaround
-buildah run $ctr touch /var/cache/apt/pkgcache.bin /var/cache/apt/srcpkgcache.bin
-
 img=$(buildah commit --rm $ctr dng-converter)
-buildah tag $img dng-converter:"$(printf '%s' "$VERSION" | tr _ .)"
+buildah tag $img dng-converter:"$VERSION"
